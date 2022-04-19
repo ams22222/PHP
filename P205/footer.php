5ba9789f9ea8d1b932ab26427a0e96bec5df5bb4
ams22222 <100142641+ams22222@users.noreply.github.com>
2022-04-19 15:57:06 +0200

    Delete P205 directory

--- P205/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="bg-light text-center text-lg-start" style="position: fixed;bottom: 0;right:0;">
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2022 Copyright: DAM_1 Abel
-  </div>
-  <!-- Copyright -->
-</footer>
-
