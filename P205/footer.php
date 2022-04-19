1799c49c38c9f19079ea78b94826f668c4b06e10
ams22222 <100142641+ams22222@users.noreply.github.com>
2022-04-19 15:47:54 +0200

    Add files via upload

---
+++ P205/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-light text-center text-lg-start" style="position: fixed;bottom: 0;right:0;">
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright: DAM_1 Abel
+  </div>
+  <!-- Copyright -->
+</footer>
+
